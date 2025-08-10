eb7d0ddeb6d21fe1ddcbfb7a2306db530855a013
Oliver Molnar <oliver.molnar@420cloud.io>
2025-08-11 01:34:19 +0200

    refactor: readability

--- src/client/App.client.tsx
+++ src/client/App.client.tsx
@@ -4,18 +4,31 @@ import { default as Component } from "./App";
 // Optionally: import your app's CSS
 // import "./styles.css";
 
-const elem = document.getElementById("root")!;
+const elem = document.getElementById("root");
+if (!elem) {
+  throw new Error("Root element not found. Ensure your HTML has a <div id='root'></div>.");
+}
+
+const getRootElement = () => {
+  if (import.meta.hot) {
+    // With hot module reloading, `import.meta.hot.data` is persisted.
+    const root = (import.meta.hot.data.root ??= createRoot(elem));
+    return root;
+  } else {
+    // The hot module reloading API is not available in production.
+    return createRoot(elem);
+  }
+}
+
 const app = (
   <StrictMode>
     <Component />
   </StrictMode>
 );
 
-if (import.meta.hot) {
-  // With hot module reloading, `import.meta.hot.data` is persisted.
-  const root = (import.meta.hot.data.root ??= createRoot(elem));
+const mount = () => {
+  const root = getRootElement();
   root.render(app);
-} else {
-  // The hot module reloading API is not available in production.
-  createRoot(elem).render(app);
 }
+
+mount()
