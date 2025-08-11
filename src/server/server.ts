44de83b8785dafdcbdba5cb1b66fe69763c964b6
Oliver Molnar <oliver.molnar@420cloud.io>
2025-08-11 02:39:30 +0200

    remove unused api endpoints

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,25 +15,6 @@ const server = serve({
 		// ** HTML imports **
 		// Bundle & route App.html to "/". This uses HTMLRewriter to scan the HTML for `<script>` and `<link>` tags, run's Bun's JavaScript & CSS bundler on them, transpiles any TypeScript, JSX, and TSX, downlevels CSS with Bun's CSS parser and serves the result.
 		"/": homepage,
-
-		// ** API endpoints ** (Bun v1.2.3+ required)
-		"/api/users": {
-			async GET(req) {
-				const users = await sql`SELECT * FROM users`;
-				return Response.json(users);
-			},
-			async POST(req) {
-				const { name, email } = await req.json();
-				const [user] =
-					await sql`INSERT INTO users (name, email) VALUES (${name}, ${email})`;
-				return Response.json(user);
-			},
-		},
-		"/api/users/:id": async (req) => {
-			const { id } = req.params;
-			const [user] = await sql`SELECT * FROM users WHERE id = ${id}`;
-			return Response.json(user);
-		},
 	},
 
 	// Enable development mode for:
